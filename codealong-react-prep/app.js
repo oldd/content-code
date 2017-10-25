7de8dc911a2073449806def56e9fc3fb6f237399
colevandersWands <evan.t.cole@gmail.com>
2017-10-25 13:16:58 +0200

    committing all - content-code

---
+++ codealong-react-prep/app.js
@@ -0,0 +1,10 @@
+// set up each app on their correct server ports
+
+var static_server = require('./config/static_server');
+var static_app = require('./services/static_app');
+static_server.use(static_app);
+
+var data_app = require('./config/data_server');
+var data_api = require('./services/data_api');
+data_app.use(data_api);
+
